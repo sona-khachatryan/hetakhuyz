09548e0f6fdcb36ca98b02885d0e32ea17f3bd55
Sona Khachatryan <sona.khachatryannn@gmail.com>
2024-06-27 15:19:26 +0400

    fetching data with ids from BE

--- src/components/sections/armenia/Armenia.jsx
+++ src/components/sections/armenia/Armenia.jsx
@@ -5,28 +5,33 @@ import axios from "axios"
 import { Link } from "react-router-dom"
 import { address } from "../../../repetitiveVariables/variables"
 import ArticleSubsection from "../article/ArticleSubsection"
+import {getNewsBySectionId, getSections, getSubsections} from "../../../api/fetchData.js";
 
 const Armenia = () => {
-    const [data,setData] = useState()
-    const [dataMilitary,setDataMilitary] = useState([])
-    const [dataPolitics,setDataPolitics] = useState([])
-    const [dataLegal,setDataLegal] = useState([])
-    const [dataSociety,setDataSociety] = useState([])
-    
-    
+    const [data,setData] = useState([]);
+    const [dataMilitary,setDataMilitary] = useState([]);
+    const [dataPolitics,setDataPolitics] = useState([]);
+    const [dataLegal,setDataLegal] = useState([]);
+    const [dataSociety,setDataSociety] = useState([]);
+    const [subsections, setSubsections] = useState();
+
+
     useEffect(()=>{
       (async () => {
         try {
-          const {data}= await axios.get(`${address}/news/filter?countryId=1`) 
-          const politics = await axios.get(`${address}/news/filter?countryId=1&categoryId=1`) 
-          const legal = await axios.get(`${address}/news/filter?countryId=1&categoryId=2`) 
-          const military = await axios.get(`${address}/news/filter?countryId=1&categoryId=3`) 
-          const society = await axios.get(`${address}/news/filter?countryId=1&categoryId=4`) 
-          Array.isArray(data) && setData(data.filter((data)=>data.newsContent.file.isImage))
-          Array.isArray(politics.data) && setDataPolitics(politics.data.filter((data)=>data.newsContent.file.isImage))
-          Array.isArray(military.data) && setDataMilitary(military.data.filter((data)=>data.newsContent.file.isImage))
-          Array.isArray(legal.data) && setDataLegal(legal.data.filter((data)=>data.newsContent.file.isImage))
-          Array.isArray(society.data) && setDataSociety(society.data.filter((data)=>data.newsContent.file.isImage))
+            const subsections = await getSubsections();
+            const {mainSections} = await getSections();
+            const armenia = mainSections.find(section => section.title === 'Հայաստան');
+            const data = await getNewsBySectionId(armenia.id);
+            const politics = await getNewsBySectionId(armenia.id, subsections.find(section => section.title === 'Քաղաքական').id);
+          const legal = await getNewsBySectionId(armenia.id, subsections.find(section => section.title === 'Իրավական').id);
+          const military = await getNewsBySectionId(armenia.id, subsections.find(section => section.title === 'Ռազմական').id);
+          const society = await getNewsBySectionId(armenia.id, subsections.find(section => section.title === 'Հասարակություն').id);
+          Array.isArray(data) && setData(data.filter((data)=>data?.newsContent?.file?.isImage))
+          Array.isArray(politics) && setDataPolitics(politics.filter((data)=>data?.newsContent?.file?.isImage))
+          Array.isArray(military) && setDataMilitary(military.filter((data)=>data?.newsContent?.file?.isImage))
+          Array.isArray(legal) && setDataLegal(legal.filter((data)=>data?.newsContent?.file?.isImage))
+          Array.isArray(society) && setDataSociety(society.filter((data)=>data?.newsContent?.file?.isImage))
         } catch (error) {
           console.log(error)
         }
@@ -39,24 +44,24 @@ const Armenia = () => {
   }
   
   return (
-        <main className="region_page">
-        <div className="region_title_container">
-            <h2>Հայաստան</h2>
-            <hr/>
-            <ul>
-              <li><Link onClick={()=>handleTop("politics")}  to={{ pathname: '/armenia',hash: '#politics' }}>Քաղաքական</Link></li>
-              <li><Link onClick={()=>handleTop('legal')} to={{ pathname: '/armenia', hash:'#legal' }}>Իրավական</Link></li>
-              <li><Link onClick={()=>handleTop('military')} to={{ pathname: '/armenia',hash: '#military' }}>Ռազմական</Link></li>
-              <li><Link onClick={()=>handleTop('society')} to={{ pathname: '/armenia', hash:'#society' }}>Հասարակություն</Link></li>
-            </ul>
-        </div>
-        <hr/>
-        <AllNews title="Թարմ Նորություններ" data={data && data}/>
-         {data && <ArticleSubsection title="Քաղաքական" to="politics" data={dataPolitics}/>}
-         {data && <ArticleSubsection title="Իրավական" to="legal" data={dataLegal}/>}
-         {data && <ArticleSubsection title="Ռազմական" to="military" data={dataMilitary}/>}
-         {data && <ArticleSubsection title="Հասարակություն" to="society" data={dataSociety}/>}
-    </main>
+      <main className="region_page">
+          <div className="region_title_container">
+              <h2>Հայաստան</h2>
+              <hr/>
+              <ul>
+                  <li><Link onClick={()=>handleTop("politics")}  to={{ pathname: '/armenia',hash: '#politics' }}>Քաղաքական</Link></li>
+                  <li><Link onClick={()=>handleTop('legal')} to={{ pathname: '/armenia', hash:'#legal' }}>Իրավական</Link></li>
+                  <li><Link onClick={()=>handleTop('military')} to={{ pathname: '/armenia',hash: '#military' }}>Ռազմական</Link></li>
+                  <li><Link onClick={()=>handleTop('society')} to={{ pathname: '/armenia', hash:'#society' }}>Հասարակություն</Link></li>
+              </ul>
+          </div>
+          <hr/>
+          <AllNews title="Թարմ Նորություններ" data={data && data}/>
+          {dataPolitics && <ArticleSubsection title="Քաղաքական" to="politics" data={dataPolitics}/>}
+          {dataLegal && <ArticleSubsection title="Իրավական" to="legal" data={dataLegal}/>}
+          {dataMilitary && <ArticleSubsection title="Ռազմական" to="military" data={dataMilitary}/>}
+          {dataSociety && <ArticleSubsection title="Հասարակություն" to="society" data={dataSociety}/>}
+      </main>
   )
 }
 
